e0e53d00af7afef15e1b6f5a36bb520733563e53
wqh0109663 <32165101+wqh0109663@users.noreply.github.com>
2018-02-06 10:51:58 +0800

    第3次提交

---
+++ org/entity/Car.java
@@ -0,0 +1,36 @@
+package org.entity;
+
+/**
+ * Created by wqh on 2018/2/6.
+ */
+public class Car {
+    private String carName;
+    private int price;
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carName='" + carName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+
+
+}
---
+++ org/entity/TestCar.java
@@ -0,0 +1,21 @@
+package org.entity;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by wqh on 2018/2/6.
+ */
+public class TestCar {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean2.xml");
+        Car car2 = (Car)applicationContext.getBean("car2");
+        //Car car1 = (Car)applicationContext.getBean("car");
+        //System.out.println(car1.toString());
+        System.out.println(car2.toString());
+    }
+
+
+
+
+}
