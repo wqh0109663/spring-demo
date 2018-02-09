da1f0c4d687cb0876080b997de45bba9d5e51482
wqh0109663 <32165101+wqh0109663@users.noreply.github.com>
2018-02-09 21:32:03 +0800

    第15次提交  aop环绕和后置增强

--- aop/test/NewPerson.java
+++ aop/test/NewPerson.java
@@ -1,11 +1,29 @@
 package aop.test;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * Created by wqh on 2018/2/9.
  */
 
 public class NewPerson {
-    public  void  beforeEat(){
+    //前置增强
+    public void beforeEat() {
         System.out.println("beforeEat。。。。");
+    }
+
+    //环绕增强
+    public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        //方法之前
+        System.out.println("执行之前");
+
+        //被增强的方法
+        proceedingJoinPoint.proceed();
+
+
+        //方法之后
+        System.out.println("执行之后");
+
+
     }
 }
